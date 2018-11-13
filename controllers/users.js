cc7ccf8a0b5b6be5924368636f302c4371aaa566
Salman Khan <im.solomon@outlook.com>
2018-11-13 20:16:20 +0500

    user general details

--- controllers/users.js
+++ controllers/users.js
@@ -21,12 +21,12 @@ module.exports = {
       .then((user) => {
         const responseObj = {
           'data':
-                    {
-                      'userName' : user.userName,
-                      'firstName': user.firstName,
-                      'lastName' : user.lastName,
-                      'email'    : user.email
-                    },
+          {
+            'userName' : user.userName,
+            'firstName': user.firstName,
+            'lastName' : user.lastName,
+            'email'    : user.email
+          },
           'message': 'Successfully Created User'
         };
         return response.status(200).send(responseObj);
@@ -42,22 +42,33 @@ module.exports = {
         response.status(500).send();
       });
   },
-
   'login': (req, response) => {
     const userBody = req.body;
+    const userName = userBody.userName;
+    const userPassword = userBody.password;
     logger.log('Post login call for user');
-    return models.Users.findOne({'where': {'userName': userBody.userName.trim()}})
-      .then((user) => {
-        const tokenString = auth.issueToken(user.userName, 'user', req.hostname);
-        const responseObj = {
-          'data':
-                    {'apiKey': tokenString},
-          'message': 'Successfully logged in User'
-        };
-        return response.status(200).send(responseObj);
+
+    return models.Users.findOne({'where': {'userName': userName.trim()}, 'rejectOnEmpty': true})
+      .then((user) =>
+        bcrypt.compareSync(userPassword, user.password)
+      )
+      .then((validUser) => {
+        if (validUser) {
+          const tokenString = auth.issueToken(userName, 'user', req.hostname);
+          const responseObj = {
+            'data':
+              {'apiKey': tokenString},
+            'message': 'Successfully logged in User'
+          };
+          return response.status(200).send(responseObj);
+        }
+        throw invalidUsernameOrPasswordError();
       })
       .catch(models.Sequelize.EmptyResultError, (err) => {
-        response.status(404).send(invalidData(err, 404));
+        response.status(404).send(InvalidUser(err, 404));
+      })
+      .catch(invalidUsernameOrPasswordError, (err) => {
+        response.status(404).send(InvalidUser(err, 404));
       })
       .catch(models.Sequelize.ValidationError, (err) => {
         response.status(400).send(invalidData(err, 400));
@@ -67,15 +78,55 @@ module.exports = {
         response.status(500).send();
       });
   },
-
+  'updateGeneralDetails': (req, response) => {
+    const userBody = req.body;
+    const currentUser = req.auth.user;
+    logger.log('Post login call for user general detail');
+    return models.Users.findOne({'where': {'userName': currentUser.trim()}, 'rejectOnEmpty': true})
+      .then((user) =>
+        user.update({
+          'firstName'  : userBody && userBody.firstName ? userBody.firstName : user.firstName,
+          'lastName'   : userBody && userBody.lastName ? userBody.lastName : user.lastName,
+          'displayName': userBody && userBody.displayName ? userBody.displayName : '',
+          'email'      : userBody && userBody.email ? userBody.email : '',
+          'about'      : userBody && userBody.about ? userBody.about : '',
+          'gender'     : userBody && userBody.gender ? userBody.gender : user.gender,
+          'title'      : userBody && userBody.title ? userBody.title : user.title
+        }))
+      .then((user) => {
+        const responseObj = {
+          'data': {
+            'title'      : user.title,
+            'gender'     : user.gender,
+            'firstName'  : user.firstName,
+            'lastName'   : user.lastName,
+            'displayName': user.displayName,
+            'email'      : user.email,
+            'about'      : user.about
+          },
+          'message': 'Successfully Updated User General Details'
+        };
+        return response.status(200).send(responseObj);
+      })
+      .catch(models.Sequelize.EmptyResultError, (err) => {
+        response.status(404).send(InvalidUser(err, 404));
+      })
+      .catch(models.Sequelize.ValidationError, (err) => {
+        response.status(400).send(invalidData(err, 400));
+      })
+      .catch((err) => {
+        logger.error(err);
+        response.status(500).send();
+      });
+  },
   'logout': (req, response) => {
     const userBody = req.body;
     logger.log('Post logout call for user');
     return models.Users.findOne({'where': {'userName': userBody.userName.trim()}})
-      .then((user) => {
+      .then(() => {
         const responseObj = {
           'data':
-                    {'apiKey': ''},
+            {'apiKey': ''},
           'message': 'Successfully logged out user'
         };
         return response.status(200).send(responseObj);
@@ -100,3 +151,17 @@ const invalidData = (err, status) => ({
   'message': 'Invalid Input',
   'error'  : err
 });
+
+// return status body
+const InvalidUser = (err, status) => ({
+  'status' : status,
+  'code'   : err.code,
+  'message': 'Username or password is incorrect',
+  'error'  : err
+});
+
+const invalidUsernameOrPasswordError = () => {
+  const error = new Error('Username or password is incorrect');
+  error.name = 'invalidUsernameOrPasswordError';
+  return error;
+};
--- models/users.js
+++ models/users.js
@@ -1,6 +1,22 @@
 /* eslint-disable indent */
 'use strict';
 const bcrypt = require('bcrypt');
+const _ = require('lodash');
+
+const CONST = {
+  'GENDER': {
+    'Male'  : 'Male',
+    'Female': 'Female'
+  },
+  'TITLE': {
+    'Mr' : 'Mr',
+    'Mrs': 'Mrs',
+    'Mss': 'Mss',
+    'Ms' : 'Ms',
+    'Mx' : 'Mx',
+    'Dr' : 'Dr'
+  }
+};
 
 /*
  * Users Model
@@ -8,43 +24,63 @@ const bcrypt = require('bcrypt');
 
 module.exports = (sequelize, DataTypes) => {
   const Users = sequelize.define('Users', {
+    'userName': {
+      'type'      : DataTypes.STRING, 'primaryKey': true,
+      'validate'  : {'len': {'args': [5, 10], 'msg': 'User name must be 5 to 10 character  character long'}}
+    },
+    'password' : {'type': DataTypes.STRING},
     'firstName': {
       'type'    : DataTypes.STRING, 'validate': {
         'notEmpty': {'msg': 'Must provide first name'},
-        'max'     : {'args': [15], 'msg': 'First name must be less then 16 character'}
-      }
-    },
-    'lastName': {
-      'type'    : DataTypes.STRING, 'validate': {
-        'notEmpty': {'msg': 'Must provide first name'},
-        'max'     : {'args': [15], 'msg': 'Last name must be less then 16 character'}
+        'len'     : {'args': [1, 20], 'msg': 'First name must be less then 20 character'}
       }
     },
     'email': {
       'type'    : DataTypes.STRING, 'unique'  : true,
       'validate': {'isEmail': {'msg': 'Contact email must be email address'}}
     },
-    'userName': {
-      'type'      : DataTypes.STRING, 'primaryKey': true,
-      'validate'  : {'is': /^([a-z0-9](?:-?[a-z0-9]){0,38})$/ig}
+    'lastName': {
+      'type'    : DataTypes.STRING, 'validate': {
+        'notEmpty': {'msg': 'Must provide last name'},
+        'len'     : {'args': [1, 20], 'msg': 'Last name must be less then 20 character'}
+      }
     },
-    'password': {'type': DataTypes.STRING},
-    'about'   : {
+    'displayName': {
       'type'    : DataTypes.STRING,
-      'validate': {'max': {'args': [15], 'msg': 'About must be less then 255 character'}}
-    }
-  },
-  {
-    'hooks': {
-      'beforeCreate': (user) => {
-        // eslint-disable-next-line no-lone-blocks
-        {
-          user.password = user.password && user.password !== ''
-          ? bcrypt.hashSync(user.password, 10) : '';
+      'validate': {'len': {'args': [1, 20], 'msg': 'display name must be less then 20 character'}}
+    },
+    'about': {
+      'type'    : DataTypes.TEXT,
+      'validate': {'len': {'args': [0, 500], 'msg': 'About must be less then 500 character'}}
+    },
+    'gender': {
+      'type'    : DataTypes.ENUM(_.values(CONST.GENDER)), 'validate': {
+        'isIn': {
+          'args': [_.values(CONST.GENDER)],
+          'msg' : 'Invalid gender'
+        }
+      }
+    },
+    'title': {
+      'type'    : DataTypes.ENUM(_.values(CONST.TITLE)), 'validate': {
+        'isIn': {
+          'args': [_.values(CONST.TITLE)],
+          'msg' : 'Invalid title'
         }
       }
     }
-  });
+  },
+    {
+      'hooks': {
+        'beforeCreate': (user) => {
+          // eslint-disable-next-line no-lone-blocks
+          {
+            user.password = user.password && user.password !== ''
+              ? bcrypt.hashSync(user.password, 10) : '';
+          }
+        }
+      }
+    });
 
   Users.associate = (models) => {
     Users.hasMany(models.ContactsPersonals, {'foriegnKey': 'userName', 'sourceKey': 'userName'});
